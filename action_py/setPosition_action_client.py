4789ef8bb809757b3bb2d923214482feaaa56f83
Gabriel Souza <gabriel_souza08@usp.br>
2022-05-31 12:28:32 -0300

    :x: added setPosition files

---
+++ action_py/setPosition_action_client.py
@@ -0,0 +1,53 @@
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+from action_interface import SetPosition
+
+
+class SetPositionActionClient(Node):
+
+    def __init__(self):
+        super().__init__('setPositiont_action_client')
+        self._action_client = ActionClient(self, SetPosition, 'setPositiont')
+
+    def send_goal(self, position_request):
+        goal_msg = SetPosition.Goal()
+        goal_msg.position_request = position_request
+
+        self._action_client.wait_for_server()
+
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: '.format(result.position_result))
+        rclpy.shutdown()
+
+
+#receive the initial postion of the drone 
+def main(args=None):
+    rclpy.init(args=args)
+
+    action_client = SetPositionActionClient()
+
+    future = action_client.send_goal(10, 10, 3)
+
+    rclpy.spin_until_future_complete(action_client, future)
+
+
+if __name__ == '__main__':
+    main()
---
+++ action_py/setPosition_action_server.py
@@ -0,0 +1,55 @@
+import math
+import time
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+
+from action_interface import SetPosition
+TOL = 0.3
+
+class SetPositionActionServer(Node):
+
+    def __init__(self):
+        super().__init__('setPosition_action_server')
+        self._action_server = ActionServer(
+            self,
+            SetPosition,
+            'setPosition',
+            self.execute_callback)
+
+    
+    # Need to get the initial position from MAV2 #
+    def execute_callback(self, goal_handle, drone_position, speed):
+        self.get_logger().info('Executing positioning..')
+        
+        feedback_msg = SetPosition.Feedback()
+        feedback_msg.partial_position = drone_position
+        
+
+        angle = math.atan2((goal_handle.request.position_resquest[1]- drone_position[1]), (goal_handle.request.position_resquest[0]- drone_position[0]))
+        
+        while (math.sqrt((goal_handle.request.position_resquest[1]- drone_position[1])**2 + (goal_handle.request.position_resquest[0]- drone_position[0])**2) >= TOL):
+           drone_position[1] += speed*math.cos(angle); 
+           drone_position[0] += speed*math.sin(angle); 
+           feedback_msg.partial_position = drone_position
+           self.get_logger().info('Feedback: '.format(feedback_msg.partial_position))
+           goal_handle.publish_feedback(feedback_msg)
+           time.sleep(1)
+
+        goal_handle.succeed()
+        
+        result = SetPosition.Result()
+        result.position_result = feedback_msg.partial_position
+        return result
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    setPositionaction_server = SetPositionActionServer()
+
+    rclpy.spin(setPositionaction_server)
+
+
+if __name__ == '__main__':
+    main()

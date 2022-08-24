1ce1e82e4926cd560ff6a5b77361679230e991e3
pedro-fuoco <pedrofuoco6@gmail.com>
2022-08-24 09:13:58 -0300

    launch files for imav 2022

---
+++ launch/mavros_indoor_70.launch.py
@@ -0,0 +1,19 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+   config = os.path.join(
+      get_package_share_directory('mavbase2'),
+      'config',
+      'mavros_indoor_70.yaml'
+      )
+
+   return LaunchDescription([
+         Node(
+            package='mavros',
+            executable='mavros_node',
+            parameters=[config]
+         )
+      ])
